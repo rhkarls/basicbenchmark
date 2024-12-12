f9945db3f6bdbf763df035b53fd6d6ef676c0fb1
Reinert Huseby Karlsen <2330659+rhkarls@users.noreply.github.com>
2024-12-12 15:00:13 +0100

    Add package version attribute

--- src/basicbenchmark/__init__.py
+++ src/basicbenchmark/__init__.py
@@ -1,2 +1,6 @@
+import importlib.metadata
+
 from basicbenchmark.benchmark import benchmark as benchmark
 from basicbenchmark.benchmark import benchmark_stats as benchmark_stats
+
+__version__ = importlib.metadata.version("basicbenchmark")
